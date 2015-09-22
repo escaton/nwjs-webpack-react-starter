09e28669de1812a5f6e66fb892b99dfa737693dd
Boyma Fahnbulleh <boymanjor@icloud.com>
2015-09-22 14:43:06 -0400

    added basic app init method

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
-_.each([1, 2, 3], function(num) {
-	console.log(num)
+import app from 'ampersand-app'
+
+window.app = app
+
+app.extend({
+	init () {
+		console.log('Begin app initialization...')
+		console.log('App initialization complete.')
+	}
 })
+
+app.init()
